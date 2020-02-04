f82ef762e82f9b3c48b1f0b2ac75371f33b84426
Young Jae Kim <byeongal@kookmin.ac.kr>
2020-02-05 00:13:18 +0900

    post_analyse (https://developers.virustotal.com/v3.0/reference#files-analyse)

--- VirusTotalVersion3.py
+++ VirusTotalVersion3.py
@@ -17,6 +17,13 @@ class PublicAPI:
                 files = data)
             return response.json()
 
+    def post_analyse(self, id):
+        response = requests.post(
+            url='https://www.virustotal.com/api/v3/files/{}/analyse'.format(id),
+            headers={'x-apikey': self.api_key},
+        )
+        return response.json()
+
 class PrivateAPI(PublicAPI):
     def __init__(self, api_key):
         super(PrivateAPI, self).__init__(api_key)
