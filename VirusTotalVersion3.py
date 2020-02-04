4645da67a11f85861770f89b01068541988d374d
Young Jae Kim <byeongal@kookmin.ac.kr>
2020-02-05 00:09:45 +0900

    post_files (https://developers.virustotal.com/v3.0/reference#files-scan)

---
+++ VirusTotalVersion3.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+class PublicAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def post_files(self, file_path):
+        if not os.path.isfile(file_path):
+            raise FileExistsError
+        else:
+            with open(file_path, 'rb') as f:
+                data = {'file': f.read()}
+            response = requests.post(
+                url = 'https://www.virustotal.com/api/v3/files',
+                headers = {'x-apikey': self.api_key},
+                files = data)
+            return response.json()
+
+class PrivateAPI(PublicAPI):
+    def __init__(self, api_key):
+        super(PrivateAPI, self).__init__(api_key)
